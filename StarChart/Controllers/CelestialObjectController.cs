0e8d3d7b6734a2e5674f00128499b84935398906
Sanford, Jeffrey <jeffrey.sanford@accenture.com>
2018-11-26 09:37:06 -0700

    Finished Module 4

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -16,5 +16,39 @@ namespace StarChart.Controllers
         {
             _context = context;
         }
-    }
+
+        [HttpGet("{id:int", nameof = "GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialObject = _context.CelestialObjects.find(id);
+            if (celestialObject == null)
+                return NotFound();
+            celestialObject.Salellites = _context.CelestialObjects.Where(e => e.OribitObjectId == id).ToList();
+            return Ok(celestialObject);
+        }
+
+        [HttpGet("{name")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = _context.CelestialObjects.Where(e => e.Name == name).toList();
+            if (!celestialObjects.Any())
+                return NotFound();
+            foreach (var celestialObject in celestialObjects)
+            {
+                celestialObject.Satallites = _context.CelestialObjects.Where(e => e.OribitObjectId == id).ToList();
+
+            }
+            return OK(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = _context.CelestialObjects.ToList();
+            foreach(var celestialObject in celestialObjects)
+            {
+                celestialObject.Satallites = _context.CelestialObjects.Where(e => e.OribitObjectId == id).ToList();
+                return Ok(celestialObjects);
+
+            }
 }
