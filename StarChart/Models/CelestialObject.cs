294acd747a6a9e27566e8fc5fb8b66a83f94c2d0
Sanford, Jeffrey <jeffrey.sanford@accenture.com>
2018-11-26 08:16:30 -0700

    Finished Module 3

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -9,14 +9,14 @@ namespace StarChart.Models
 {
     public class CelestialObject
     {
-        public int Id {get; set; }
+        public int Id { get; set; }
         [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
 
         [NotMapped]
         public List<CelestialObject> Satellites { get; set; }
-        public TimeSpan OrbitalPeriod {get; set; }
+        public TimeSpan OrbitalPeriod { get; set; }
 
     }
 }
